cb002371b93b6c1ec51dc1cdb13163905cecf7c0
Mostafa Mahmoud <mostafa.mahmoud@g-beehive.com>
2012-06-29 22:27:19 +0200

    Tests for the cosine rules similarity computation function.

--- tests.rb
+++ tests.rb
@@ -51,4 +51,23 @@ class Tests < Test::Unit::TestCase
 
     diff.each {|val| assert_equal(val, 0)}
   end
+
+
+  def test_cosine_rule
+    users1 = [[1, 4, 5, 2, 1], [5, -1, 2, 4, 10], [42134, 123, 123], []]
+    users2 = [[0, -4, 1, 2, 1], [51, 12, 4, -101, 123], [23, 1, 12], []]
+
+    users1 << [123, 1007, 10007, 231]
+    users2 << [123, 1007, 10007, 231]
+    users1 << [123, 1007, 10007, 231]
+    users2 << [-123, -1007, -10007, -231]
+    users1 << [1, 2, 3, 4]
+    users2 << [-12, -3, 2, 3]
+
+    actual_values = [-0.18659, 0.53180899, 0.8873811, 0, 1, -1, 0]
+    expected_values = users1.zip(users2).map {|x| Similarity.cosine_rule(x[0], x[1])}
+    diff = actual_values.zip(expected_values).map {|sim| sim[0] - sim[1]}
+
+    diff.each {|val| assert(val.abs < 1e-5)}
+  end
 end
